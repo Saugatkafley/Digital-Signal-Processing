14c0da0b5214b7d5f2f4a26b83cdaa526615c02b
saugatkafley@gmail.com <saugatkafley@gmail.com>
2022-08-23 18:38:03 +0545

    removed folder

--- lab3(FFT)/cw_c.m
+++ lab3(FFT)/cw_c.m
@@ -1,9 +1,9 @@
-x = [1.2 3.7 0.6 1.3 2];
+x = [1.2 3.7 0.6 1.3 2 0 0 0];
 y = fft(x , 8);
 
 subplot(211)
 stem(y)
-title("FFT of N=5 sequence");
+title("FFT of N=5 sequence 075bct099");
 grid on;
 subplot(212)
 stem(x);
